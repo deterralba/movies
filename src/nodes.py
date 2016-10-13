1c0e068caaab3ccc6173f486335c587fdcf5ccfa
deterralba <deterralba@gmail.com>
2016-10-13 17:40:42 +0200

    update Document to use slug, add Event Person Tag

--- src/nodes.py
+++ src/nodes.py
@@ -1,43 +1,44 @@
 import re
+from datetime import datetime
 from core import Link, Node, Graph, graph, db
 
 COMMENT_REGEX = r'\B(@\w+)'
 
 class Document(Node):
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, name, slug=None):
+        super().__init__(name, slug)
         self.comment = ''
 
     def get_specific_connected_nodes(self):
-        possible_nodes_names = self.parse_comment()
+        possible_nodes_slugs = self.parse_comment()
         existing_nodes = []
-        for node_name in possible_nodes_names:
-            node = graph.get_node(node_name)
+        for node_slug in possible_nodes_slugs:
+            node = graph.get_node_by_slug(node_slug)
             if node is not None:
                 existing_nodes.append(node)
         print('EXISTING nodes {}'.format(existing_nodes))
         return existing_nodes
 
     def parse_comment(self):
-        nodes_names = re.findall(COMMENT_REGEX, self.comment)
-        nodes_names = [name[1:] for name in nodes_names]
-        nodes_names = list(set(nodes_names))
-        print('FOUND nodes in comments {}'.format(nodes_names))
-        return nodes_names
+        nodes_slugs = re.findall(COMMENT_REGEX, self.comment)
+        nodes_slugs = [slug[1:] for slug in nodes_slugs]
+        nodes_slugs = list(set(nodes_slugs))
+        print('FOUND nodes in comments {}'.format(nodes_slugs))
+        return nodes_slugs
 
 
 class TestNode(Node):
     def get_specific_connected_nodes(self):
-        node_name = input('> ')
+        node_slug = input('> ')
         l = []
         print('Existing nodes: {}'.format(graph.nodes))
-        while node_name != '':
-            node = graph.get_node(node_name)
+        while node_slug != '':
+            node = graph.get_node_by_slug(node_slug)
             if node is not None:
                 l.append(node)
             else:
                 print('Node could not be found')
-            node_name = input('> ')
+            node_slug = input('> ')
         return l
 
 class Film(Document):
@@ -49,4 +50,13 @@ class Book(Document):
 class Article(Document):
     pass
 
+class Event(Node):
+    def __init__(self, name, slug=None):
+        super().__init__(name, slug)
+        self.date = datetime.now()
 
+class Person(Node):
+    pass
+
+class Tag(Node):
+    pass
